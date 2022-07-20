7e22de05b0e96915ee9590b7c54f0b466c68d10d
myauxcode <104797580+myauxcode@users.noreply.github.com>
2022-07-20 17:27:51 -0400

    Add files via upload

---
+++ EXAMENSDNE - copia/EXAMENSDNE/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form1
+
+
+
+    Public Function existe_dig(n As Integer, dig As Integer) As Boolean
+        Dim ban As Boolean
+        Dim d As Integer
+        ban = False
+
+        While n > 0 And ban = False
+
+            d = n Mod 10
+            n = n \ 10
+
+            If d = dig Then
+                ban = True
+
+            End If
+
+
+        End While
+
+        Return ban
+
+    End Function
+
+
+    Private Sub PREGUNTA1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PREGUNTA1ToolStripMenuItem.Click
+        TextBox2.Text = existe_dig(TextBox1.Text, TextBox2.Text)
+    End Sub
+
+
+
+End Class
