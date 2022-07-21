f9c5f73a71917ff52afc8bc05d50796a13ec2faf
myauxcode <104797580+myauxcode@users.noreply.github.com>
2022-07-20 20:27:16 -0400

    Add files via upload

---
+++ TAREAVACIA/EXAMEN GRUPO SC/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Pregunta1ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Pregunta2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(345, 94)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(345, 169)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(610, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Pregunta1ToolStripMenuItem, Me.Pregunta2ToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "Menu"
+        '
+        'Pregunta1ToolStripMenuItem
+        '
+        Me.Pregunta1ToolStripMenuItem.Name = "Pregunta1ToolStripMenuItem"
+        Me.Pregunta1ToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.Pregunta1ToolStripMenuItem.Text = "Pregunta 1"
+        '
+        'Pregunta2ToolStripMenuItem
+        '
+        Me.Pregunta2ToolStripMenuItem.Name = "Pregunta2ToolStripMenuItem"
+        Me.Pregunta2ToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.Pregunta2ToolStripMenuItem.Text = "Pregunta 2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(610, 349)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Pregunta1ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Pregunta2ToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ TAREAVACIA/EXAMEN GRUPO SC/Form1.vb
@@ -0,0 +1,129 @@
+﻿Public Class Form1
+    'pregunta 1
+
+    Public Function Veri_full(n As Integer) As Boolean
+        Return ((frec_dig(n, Dig_may(n)) = 3) And (frec_dig(n, Dig_men(n)) = 2)) Or
+            ((frec_dig(n, Dig_may(n)) = 2) And (frec_dig(n, Dig_men(n)) = 3))
+
+    End Function
+    Public Function Dig_men(n As Integer) As Integer
+        Dim dm, d As Integer
+        dm = n Mod 10
+        n = n \ 10
+
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+
+            If d < dm Then
+                dm = d
+            End If
+        End While
+        Return dm
+
+    End Function
+
+    Public Function Dig_may(n As Integer) As Integer
+        Dim d1, d2 As Integer
+
+        d1 = n Mod 10
+        n = n \ 10
+
+        While n > 0
+            d2 = n Mod 10
+            n = n \ 10
+
+            If d2 > d1 Then
+                d1 = d2
+            End If
+        End While
+        Return d1
+
+    End Function
+    Private Sub Pregunta1ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Pregunta1ToolStripMenuItem.Click
+        TextBox2.Text = Veri_full(TextBox1.Text)
+    End Sub
+
+    'pregunta 2
+
+    Public Function ejercicio2(n As Integer) As Integer
+        Dim d, e, nn As Integer
+
+        While n > 0
+            d = n Mod 10
+
+            If frec_dig(n, d) = 1 Then
+
+                nn = nn + (d * 10 ^ e)
+                e = e + 1
+
+            End If
+            n = Elim_todos_num(n, d)
+
+        End While
+        Return nn
+
+    End Function
+
+
+    Public Function frec_dig(n As Integer, dig As Integer) As Integer
+        Dim fd, d As Integer
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+
+            If dig = d Then
+                fd = fd + 1
+
+            End If
+        End While
+        Return fd
+
+    End Function
+
+
+
+    Public Function Elim_todos_num(n As Integer, num As Integer) As Integer 'by ANGEL
+        Dim nd, d, nn, e As Integer
+
+        nd = Cant_digs(num)
+        d = 0
+        nn = 0
+        e = 0
+
+        While n > 0
+
+            d = n Mod (10 ^ nd)
+            n = n \ (10 ^ nd)
+
+            If d <> num Then
+
+                nn = nn + (d * (10 ^ e))
+                e = e + nd
+
+            End If
+        End While
+
+        Return nn
+    End Function
+
+    Public Function Cant_digs(n As Integer) As Integer
+        Dim cd, d As Integer
+        cd = 0
+        If n = 0 Then
+            n = 1
+        End If
+        While n > 0
+            d = n Mod 10
+            n = n \ 10
+            cd = cd + 1
+        End While
+        Return cd
+    End Function
+
+
+
+    Private Sub Pregunta2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Pregunta2ToolStripMenuItem.Click
+        TextBox2.Text = ejercicio2(TextBox1.Text)
+    End Sub
+End Class
